14ad85aeabc852a61c276e50ff4754f7a5875c2e
James Hodgkinson <james@terminaloutcomes.com>
2023-06-04 09:06:19 +1000

    basic filename handling bits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+//! <https://houqp.github.io/leptess/leptess/index.html>
+
 fn main() {
+
+    // get the filename from the first argv
+    let filename = match std::env::args().nth(1){
+        Some(val) => val,
+        None => {
+            eprintln!("no filename given");
+            return;
+        }
+    };
+
+    // if the file doesn't exist then exit
+    if !std::path::Path::new(&filename).exists() {
+        eprintln!("file {} does not exist", filename);
+        return;
+    }
+
     let mut lt = match leptess::LepTess::new(
         Some("/opt/homebrew/Cellar/tesseract/5.3.1/share/tessdata"),
         "eng",
@@ -6,7 +24,7 @@ fn main() {
         Ok(lt) => lt,
         Err(err) => panic!("failed to load tessdata: {:?}", err),
     };
-    lt.set_image("./tests/tweet-home-depot.png").unwrap();
+    lt.set_image(filename).unwrap();
     let text = match lt.get_utf8_text() {
         Ok(text) => text,
         Err(err) => {
@@ -14,5 +32,19 @@ fn main() {
             return;
         }
     };
+
+    let ok_chars = [
+        '@',
+        '\'',
+        '"',
+        '‘',
+        '’',
+    ];
+    // strip all letter characters but leave in @
+    let text = text
+        .chars()
+        .filter(|c| c.is_alphanumeric() || ok_chars.contains(c) || c.is_whitespace() )
+        .collect::<String>();
+
     println!("{}", text.replace('\n', " "));
 }
