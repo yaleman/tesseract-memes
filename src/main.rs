66e2c7c84116ee708c4c3e3399210f1f463f1721
James Hodgkinson <james@terminaloutcomes.com>
2023-08-05 12:27:22 +1000

    automagically looking for tesseract

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 //! <https://houqp.github.io/leptess/leptess/index.html>
 
-fn main() {
+use std::path::Path;
 
+fn get_tesseract_search_paths() -> Vec<String> {
+    let mut search_places: Vec<String> = vec![("./".to_string())];
+    if cfg!(target_os = "macos") {
+        search_places.push("/opt/homebrew/Cellar/tesseract/".to_string());
+    };
+    search_places
+}
+
+fn find_tesseract_path() -> Option<String> {
+    for place in get_tesseract_search_paths() {
+        if !Path::new(&place).exists() {
+            continue;
+        }
+        // search the place for a tesseract folder
+        for path in glob::glob(&format!("{}/**", place)).unwrap() {
+            if let Ok(path) = path {
+                if path.ends_with("share/tessdata") {
+                    return Some(path.to_str().unwrap().to_string());
+                };
+            }
+        }
+    }
+
+    None
+}
+
+fn main() {
     // get the filename from the first argv
-    let filename = match std::env::args().nth(1){
+    let filename = match std::env::args().nth(1) {
         Some(val) => val,
         None => {
             eprintln!("no filename given");
@@ -17,10 +44,13 @@ fn main() {
         return;
     }
 
-    let mut lt = match leptess::LepTess::new(
-        Some("/opt/homebrew/Cellar/tesseract/5.3.1/share/tessdata"),
-        "eng",
-    ) {
+    let tesspath = find_tesseract_path();
+    if tesspath.is_none() {
+        eprintln!("tesseract not found, please install it, looked for 'share/tessdata' in the following places: {}", get_tesseract_search_paths().join(", "));
+        return;
+    }
+
+    let mut lt = match leptess::LepTess::new(Some(&tesspath.unwrap()), "eng") {
         Ok(lt) => lt,
         Err(err) => panic!("failed to load tessdata: {:?}", err),
     };
@@ -33,17 +63,11 @@ fn main() {
         }
     };
 
-    let ok_chars = [
-        '@',
-        '\'',
-        '"',
-        '‘',
-        '’',
-    ];
+    let ok_chars = ['@', '\'', '"', '‘', '’'];
     // strip all letter characters but leave in @
     let text = text
         .chars()
-        .filter(|c| c.is_alphanumeric() || ok_chars.contains(c) || c.is_whitespace() )
+        .filter(|c| c.is_alphanumeric() || ok_chars.contains(c) || c.is_whitespace())
         .collect::<String>();
 
     println!("{}", text.replace('\n', " "));
